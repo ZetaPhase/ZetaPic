1b92da75c6ff6e4c997f8c8c30d4193cda501ebc
Dave Ho <daveho8888@gmail.com>
2016-04-30 06:02:17 +0000

    add stuff

--- ZetaPic/HttpServer/MainHttpServer.py
+++ ZetaPic/HttpServer/MainHttpServer.py
@@ -1,37 +1,40 @@
 import time
 import BaseHTTPServer
+import os
 
-HOST_NAME = '192.168.1.78' # !!!REMEMBER TO CHANGE THIS!!!
-PORT_NUMBER = 80 # Maybe set this to 9000.
-
+HOST_NAME = os.getenv("IP", "0.0.0.0") # c9 Listen IP
+PORT_NUMBER = int(os.getenv("PORT", 8080)) #c9 listen port
+DOCUMENT_ROOT = "htdocs"
 
 class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
-    def do_POST(s):
-        s.send_response(200)
-        s.send_header("Content-type", "text/html")
-        s.send_header("random", "Hello World")
-        s.end_headers()
-        s.wfile.write("<html><body><p>Thank you for your submission.</p></body></html>")
-        print s.rfile
+    def do_POST(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.send_header("random", "Hello World")
+        self.end_headers()
+        self.wfile.write("<html><body><p>Thank you for your submission.</p></body></html>")
+        #print self.rfile
         
-    def do_GET(s):
-        s.send_response(200)
-        s.send_header("Content-type", "text/html")
-        s.send_header("random", "Hello World")
-        s.end_headers()
-        s.wfile.write("<html><head><title>Title goes here.</title></head>")
-        s.wfile.write("<body><p>This is a test.</p>")
-        s.wfile.write("<a href='http://www.google.com'>Google</a>")
-        s.wfile.write("<form action='demo_form.asp' method='get' id='form1'> First name: <input type='text' name='fname'><br>Last name: <input type='text' name='lname'><br><input type='submit' value='Submit'></form>")
-        s.wfile.write("<p>You accessed path: %s</p>" % s.path)
-        s.wfile.write("</body></html>")
-        print s.params
-        print s.path
-        print s.headers.getheaders('User-Agent')
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.send_header("random", "Hello World")
+        self.end_headers()
+        self.wfile.write("<html><head><title>Title goes here.</title></head>")
+        self.wfile.write("<body><p>This is a test.</p>")
+        self.wfile.write("<a href='http://www.google.com'>Google</a>")
+        self.wfile.write("<form action='demo_form.asp' method='get' id='form1'> First name: <input type='text' name='fname'><br>Last name: <input type='text' name='lname'><br><input type='submit' value='Submit'></form>")
+        self.wfile.write("<p>You accessed path: %s</p>" % self.path)
+        self.wfile.write("</body></html>")
+        #print self.params
+        print self.path
+        print self.headers.getheaders('User-Agent')
 
 httpd = BaseHTTPServer.HTTPServer((HOST_NAME, PORT_NUMBER), MyHandler)
-try:
-    httpd.serve_forever()
-except KeyboardInterrupt:
-    pass
-httpd.server_close()
+
+if __name__ == '__main__':
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
