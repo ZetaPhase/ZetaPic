147b3b3ac575f1699be51e0fc53e2b60a0fe2cf7
daveho8888 <daveho8888@gmail.com>
2016-05-03 19:40:36 -0700

    grayscale conversion complete

--- ZetaPic/www/transform.py
+++ ZetaPic/www/transform.py
@@ -5,3 +5,19 @@ Created on Tue May 03 18:37:07 2016
 @author: daveho8888
 """
 
+import os
+import skimage
+from skimage import data
+from skimage import novice
+from skimage import io
+from skimage.viewer import ImageViewer
+
+def toGrayscale(image):
+    for row in range(0, len(int(grapes.shape[0]))):
+        for col in range(0, len(int(grapes.shape[1]))):
+            average = image[row, col][0] + image[row, col][1] + image[row, col][2]
+            image[row, col] = (average, average, average)
+            
+def show(image):
+    viewer = ImageViewer(image)
+    viewer.show()
