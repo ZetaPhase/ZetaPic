4577c7a13da062350da99dbf2d4d75253652e6cc
daveho8888 <daveho8888@gmail.com>
2016-05-03 21:12:13 -0700

    copy bug fixed
    copy bug has been fixed

--- ZetaPic/www/transform.py
+++ ZetaPic/www/transform.py
@@ -5,6 +5,7 @@ Created on Tue May 03 18:37:07 2016
 @author: daveho8888
 """
 
+import numpy as np
 import os
 import skimage
 from skimage import data
@@ -12,19 +13,45 @@ from skimage import novice
 from skimage import io
 from skimage.viewer import ImageViewer
 
+class Image:
+    def __init__(self, image):
+        self.image = np.copy(image)
+        self.original = np.copy(image)
+        self.width = int(image.shape[1])
+        self.height = int(image.shape[0])
+    
+    def reset(self):
+        self.image = np.copy(self.original)
+        
+    def toGrayScale(self):
+        for row in range(0, self.height):
+            for col in range(0, self.width):
+                average = (self.image[row, col][0] + self.image[row, col][1] + self.image[row, col][2])/3.0
+                self.image[row, col] = (average, average, average)
+    
+    def show(self):
+        viewer = ImageViewer(self.image)
+        viewer.show()
+
 def toGrayscale(image):
-    for row in range(0, len(int(grapes.shape[0]))):
-        for col in range(0, len(int(grapes.shape[1]))):
+    for row in range(0, int(grapes.shape[0])):
+        for col in range(0, int(grapes.shape[1])):
             average = image[row, col][0] + image[row, col][1] + image[row, col][2]
             image[row, col] = (average, average, average)
     
             
 def toInverted(image):
-    for row in range(0, len(int(grapes.shape[0]))):
-        for col in range(0, len(int(grapes.shape[1]))):
+    for row in range(0, int(grapes.shape[0])):
+        for col in range(0, int(grapes.shape[1])):
             for i in range(0, 3):
                 image[row, col][i] = 180 - int(image[row, col][i])
 
 def show(image):
     viewer = ImageViewer(image)
     viewer.show()
+    
+if __name__ == "__main__":
+    filename = os.path.join(skimage.data_dir, 'grapes.jpg')
+    grape = io.imread(filename)
+    grapes = Image(grape)
+    
